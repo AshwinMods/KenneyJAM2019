6cf250f5e6039fa409f68c24f61d5e5ba25dd4ce
Ashwani Sehrawat <AshwinMods@gmail.com>
2019-08-10 10:48:47 +0530

    Layout and Arch. Changes

---
+++ Assets/Scripts/Store.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Store : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Item
+    {
+        public string name;
+        public Sprite sprite;
+        [TextArea(1,4)]
+        string[] isseKatega;
+    }
+
+    public Item[] items;
+}
