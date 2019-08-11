314cbbef107020a6c045553258299b367d70a2ac
Ashwani Sehrawat <AshwinMods@gmail.com>
2019-08-11 20:41:14 +0530

    Complete

--- Assets/Scripts/Sarpunch.cs
+++ Assets/Scripts/Sarpunch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Sarpunch : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class Sarpunch : MonoBehaviour
         public GameObject objShadow;
         public Sprite img;
         public Image lvlBtn;
+        public bool stat;
     }
 
     public Puzzle[] puzzles;
@@ -27,7 +29,7 @@ public class Sarpunch : MonoBehaviour
     public Animation ringChk;
     public Animation ringFix;
     public AnimationClip animClear, animFill, animErr, animDone;
-    public Util_Tween ui_mainMenu, ui_lvlSel, ui_InGame;
+    public Util_Tween ui_mainMenu, ui_lvlSel, ui_InGame, ui_Tutorial, ui_YouWon;
 
     int selectedlevel = 0;
     public void OnLevelSelect(int lvl)
@@ -75,12 +77,12 @@ public class Sarpunch : MonoBehaviour
     IEnumerator Check_Process()
     {
         yield return new WaitForSeconds(1);
-        if (checkValid && rCunt.accuracy > 0.99)
+        if (checkValid && rCunt.accuracy > 0.9f)
         {
             ringChk.Stop();
             ringChk.gameObject.SetActive(false);
             ringFix.gameObject.SetActive(true);
-            ringFix.clip = animFill;
+            ringFix.clip = animDone;
             ringFix.Play();
 
             // No time for beauty, beast on duty
@@ -107,11 +109,35 @@ public class Sarpunch : MonoBehaviour
         }
     }
 
-
+    int progCounter = 0;
+    public Text progText;
+    public GameObject gameOver;
     public void OnLevelComplete()
     {
+        if (!puzzles[selectedlevel].stat)
+        {
+            progCounter++;
+            progText.text = progCounter + "";
+        }
+        puzzles[selectedlevel].stat = true;
         puzzles[selectedlevel].lvlBtn.sprite = puzzles[selectedlevel].img;
-        ui_InGame.Close_And_Disable();
-        ui_lvlSel.gameObject.SetActive(true);
+
+        if (progCounter == 6)
+        {
+            gameOver.SetActive(true);
+        }
+        else
+        {
+            ui_YouWon.gameObject.SetActive(true);
+        }
+
+        
+        //ui_InGame.Close_And_Disable();
+        //ui_lvlSel.gameObject.SetActive(true);
+    }
+
+    public void ReStart()
+    {
+        SceneManager.LoadScene(0);
     }
 }
